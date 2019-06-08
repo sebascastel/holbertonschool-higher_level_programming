f7be2c03bc01c055fab490d13c08a92d838bff9e
sebascastel <47120815+sebascastel@users.noreply.github.com>
2019-06-08 15:57:46 -0500

    Create 12-student.py

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) == list:
+            adict = {}
+            for key in attrs:
+                if type(key) != str:
+                    return self.__dict__
+                if key in self.__dict__.keys():
+                    adict[key] = self.__dict__[key]
+            return adict
+        else:
+            return self.__dict__
